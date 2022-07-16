1bed4190eb86a49eca0dd6d16f4fdf5bebb869ce
mr-new-0509 <mr.new0509@gmail.com>
2022-07-16 14:44:23 +0900

    Welcome page

--- src/pages/WelcomePage.vue
+++ src/pages/WelcomePage.vue
@@ -1,3 +1,26 @@
 <template>
-  <h1>welcome</h1>
+  <p class="text-4xl font-bold">Hello There!</p>
+  <p class="text-md mt-6">
+    Let's buy some insurance. It is going to take only a few steps
+  </p>
+  <div class="flex justify-center mt-6">
+    <router-link
+      class="
+        border border-black
+        bg-black
+        hover:bg-white
+        rounded-none
+        text-white
+        hover:text-black
+        px-16
+        py-2
+        transition
+        ease-in
+        duration-150
+      "
+      to="/form"
+    >
+      Start
+    </router-link>
+  </div>
 </template>
